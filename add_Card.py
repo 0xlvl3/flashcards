d19cd7966d90e6e02cfd89b1ea08add669124c5e
0xlvl3 <cryptochoccy@gmail.com>
2022-12-15 12:23:49 +1100

    Updated logic

--- add_Card.py
+++ add_Card.py
@@ -1,21 +1,22 @@
+flashcards = {}
+
+
 def addCard():
     # Create a loop to iterate over the flashcards
     while True:
         # Ask the user what they want to do
         action = input(
-            "Do you want to (a)dd a flashcard, (r)emove a flashcard, or (q)uit? "
+            "Do you want to (a)dd a flashcard, (s)ee current questions and answers or (q)uit? "
         )
-
         if action.lower() == "a":
             # Ask the user for the flashcard question and answer
             question = input("Enter the flashcard question: ")
             answer = input("Enter the flashcard answer: ")
-
             # Add the flashcard to the dictionary
             flashcards[question] = answer
-        elif action.lower() == "r":
-            # Ask the user for the flashcard question
-            question = input("Enter the flashcard question: ")
-
-        else:
+        elif action.lower() == "s":
+            for question, answer in flashcards.items():
+                print("Question: " + question)
+                print("Answer: " + answer)
+        elif action.lower() == "q":
             break
--- flash_Cards.py
+++ flash_Cards.py
@@ -1,7 +1,22 @@
+import add_Card
+
 from add_Card import addCard
 import remove_Card
 
 
-flashcards = {}
-
 addCard()
+flashcards = add_Card.flashcards
+correct = 0
+incorrect = 0
+
+for question, answer in flashcards.items():
+    print(question + " ")
+    userInput = input("Type answer: ")
+    if userInput.lower() == answer.lower():
+        print("Correct!")
+        correct += 1
+    else:
+        print("Incorrect!")
+        incorrect += 2
+
+print(f"You got {correct} correct and {incorrect} incorrect")
