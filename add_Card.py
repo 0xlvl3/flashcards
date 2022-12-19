a6e51df8ad8b5b3ac280d971eaee3519df486944
0xlvl3 <cryptochoccy@gmail.com>
2022-12-19 16:19:29 +1100

    Added menu

--- add_Card.py
+++ add_Card.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 flashcards = {}
 
 
@@ -20,9 +23,35 @@ def addCard():
             answer = input("Enter the flashcard answer: ")
             # Add the flashcard to the dictionary
             flashcards[question] = answer
+
+            specifyDeck = input("Name of the deck you wish to add this card: ")
+            if os.path.isdir(specifyDeck):
+                print(f"{specifyDeck} exists")
+                print(f"Card has been added to {specifyDeck}")
+
+                with open(f"{specifyDeck}/flashcard.txt", "a") as file:
+                    json_data = json.dumps(flashcards)
+                    file.write(json_data)
+            else:
+                userInput = input(
+                    f"{specifyDeck} doesn't exist do you want to create it? Y/N"
+                ).lower()
+                if userInput.lower() == "y":
+                    os.mkdir(specifyDeck)
+                    with open(f"{specifyDeck}/flashcard.txt", "a") as file:
+                        json_data = json.dumps(flashcards)
+                        file.write(json_data)
+
+                else:
+                    print("Returning to menu")
+                    continue
+
         elif action.lower() == "s":
             for question, answer in flashcards.items():
                 print("Question: " + question)
                 print("Answer: " + answer)
         elif action.lower() == "e":
             break
+
+
+addCard()
